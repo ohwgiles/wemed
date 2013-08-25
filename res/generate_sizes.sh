ed42e4b07fd694a1d58b588640ddd0eacb307ad2
Oliver Giles <ohw.giles@gmail.com>
2013-08-25 10:17:56 +0300

    add icon

---
+++ res/generate_sizes.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+convert -density 60 icon.svg 16.png
+convert -density 82.5 icon.svg 22.png
+convert -density 90 icon.svg 24.png
+convert -density 180 icon.svg 48.png
+convert -density 240 icon.svg 64.png
