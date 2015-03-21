91036e9d872fae6868e08c6603b6f6aa0ce2ed16
Oliver Giles <ohw.giles@gmail.com>
2015-03-21 09:51:37 +0100

    new icon

--- res/generate_sizes.sh
+++ res/generate_sizes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-convert -background none -density 60 icon.svg 16.png
-convert -background none -density 82.5 icon.svg 22.png
-convert -background none -density 90 icon.svg 24.png
-convert -background none -density 120 icon.svg 32.png
-convert -background none -density 180 icon.svg 48.png
-convert -background none -density 240 icon.svg 64.png
+convert -background none -density 15 icon.svg 16.png
+convert -background none -density 20.625 icon.svg 22.png
+convert -background none -density 22.5 icon.svg 24.png
+convert -background none -density 30 icon.svg 32.png
+convert -background none -density 45 icon.svg 48.png
+convert -background none -density 60 icon.svg 64.png
