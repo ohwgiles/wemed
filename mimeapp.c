53fe36acbba5e3b746a7b702346468a21288afa6
Oliver Giles <ohw.giles@gmail.com>
2017-11-03 14:54:50 +0200

    search for preferred external app .desktop files in $HOME

--- mimeapp.c
+++ mimeapp.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include "exec.h"
 #include "mimeapp.h"
 
@@ -13,21 +14,32 @@ struct Application get_default_mime_app(const char* mimetype) {
 	char buffer[64];
 
 	const char* xdgmime[] = { "xdg-mime", "query", "default", mimetype, 0 };
-	if(exec_get(buffer, 63, "xdg-mime", xdgmime) < 0) return a;
+	if(exec_get(buffer, 63, "xdg-mime", xdgmime) < 0)
+		return a;
 	*strchrnul(buffer, '\n') = '\0';
 
-	char* path = malloc(sizeof("/usr/share/applications/") + strlen(buffer) + 1);
-	sprintf(path, "/usr/share/applications/%s", buffer);
+	struct stat st;
+
+	char* path;
+	// first look for the .desktop file in the user's home
+	asprintf(&path, "%s/.local/share/applications/%s", getenv("HOME"), buffer);
+	if(stat(path, &st) != 0) {
+		free(path);
+		// if it doesn't exist, try the system path
+		asprintf(&path, "/usr/share/applications/%s", buffer);
+	}
 
 	const char* grep_exec[] = { "grep", "^Exec=", path, 0 };
-	if(exec_get(buffer, 63, "grep", grep_exec) < 0) return free(path), a;
+	if(exec_get(buffer, 63, "grep", grep_exec) < 0)
+		return free(path), a;
 	*strchrnul(buffer, '\n') = '\0';
 
 	char executable_path[64] = {0};
 	sscanf(buffer, "Exec=%48s", executable_path);
 
 	const char* grep_name[] = { "grep", "^Name=", path, 0 };
-	if(exec_get(buffer, 63, "grep", grep_name) < 0) return free(path), a;
+	if(exec_get(buffer, 63, "grep", grep_name) < 0)
+		return free(path), a;
 	*strchrnul(buffer, '\n') = '\0';
 
 	char *executable_name = &buffer[sizeof("Name=")-1];
@@ -43,6 +55,7 @@ struct Application get_default_mime_app(const char* mimetype) {
 char* get_file_mime_type(const char* filename) {
 	char buffer[64] = {0};
 	const char* args[] = { "xdg-mime", "query", "filetype", filename, 0 };
-	if(exec_get(buffer, 63, "xdg-mime", args) < 0) return 0;
+	if(exec_get(buffer, 63, "xdg-mime", args) < 0)
+		return 0;
 	return strdup(buffer);
 }
